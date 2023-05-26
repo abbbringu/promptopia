baaf9b132b24fb0d294c3d3c9a1e77204b3ba206
abbbringu <brianphi.nguyen@gmail.com>
2023-05-26 23:11:29 +0200

    - Add profile page

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -2,10 +2,13 @@
 
 import { useState } from "react";
 import Image from 'next/image';
-import { UseSession } from "next-auth/react";
+import { useSession } from "next-auth/react";
 import { usePathname, useRouter } from 'next/navigation';
 
 function PromptCard({post, handleTagClick, handleEdit, handleDelete}) {
+  const {data: session} = useSession();
+  const pathName = usePathname();
+  const router = useRouter();
   const [copied, setCopied] = useState("");
 
   const handleCopy = () => {
@@ -53,6 +56,22 @@ function PromptCard({post, handleTagClick, handleEdit, handleDelete}) {
       >
         {post.tag}
       </p>
+      {session?.user.is === post.creator.id && pathName === '/profile' && (
+        <div className="flex-center gap-4 border-t border-gray-100 pt-3 mt-5">
+          <p 
+            className="font-inter text-sm green_gradient cursor-pointer"
+            onClick={handleEdit}
+          >
+            Edit
+          </p>
+          <p 
+            className="font-inter text-sm orange_gradient cursor-pointer"
+            onClick={handleDelete}
+          >
+            Delete
+          </p>
+        </div>
+      )}
     </div>
   )
 }
